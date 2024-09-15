9c5f586cefe66a25d9e8133a31e25c5511fb2d4e
Aleksandra <rogalevas@yandex.ru>
2024-09-15 21:51:27 +0300

    commit first home

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val settingButton = findViewById<Button>(R.id.setting_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+
+//        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity, "Нажали на картинку1!", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        searchButton.setOnClickListener(searchButtonClickListener)
+//
+//        settingButton.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на картинку2!", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        libraryButton.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на картинку3!", Toast.LENGTH_SHORT).show()
+//        }
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
-    }
+        libraryButton.setOnClickListener {
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+        settingButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+  }
 }
