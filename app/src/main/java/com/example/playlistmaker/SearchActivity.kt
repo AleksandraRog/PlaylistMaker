7bc776a793bf2ad36f06a124ba1d3a691a380149
Aleksandra <rogalevas@yandex.ru>
2024-09-18 16:28:46 +0300

    commit second home project

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,12 +1,82 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchTextValue: String = EDIT_TEXT_DEF
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_search)
+
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val backButton = findViewById<Button>(R.id.button_back)
+
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+            finish()
+        }
+
+        clearButton.setOnClickListener {
+
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                searchTextValue = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+       outState.putString("EDIT_TEXT_KEY", searchTextValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchTextValue = savedInstanceState.getString(EDIT_TEXT_KEY, EDIT_TEXT_DEF)
+    }
+
+    companion object{
+        const val EDIT_TEXT_KEY = "EDIT_TEXT_KEY"
+        const val EDIT_TEXT_DEF = ""
+    }
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,24 +1,81 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var  supportButton: ImageView
+    private lateinit var  shareButton: ImageView
+    private lateinit var  arrowButton: ImageView
+
+    private var intentValue: Intent? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<Button>(R.id.button_back)
+        supportButton = findViewById<ImageView>(R.id.button_support)
+        shareButton = findViewById<ImageView>(R.id.button_share)
+        arrowButton = findViewById<ImageView>(R.id.button_arrow)
 
         backButton.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+            finish()
         }
 
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.theme_support_message))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.text_support_message))
+            }
+            intentValue = supportIntent
+            startActivity(supportIntent)
+        }
 
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.link_course))
+            }
+            intentValue = shareIntent
+            startActivity(shareIntent)
+        }
+
+        arrowButton.setOnClickListener {
+            val arrowIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_arrow)))
+            intentValue = arrowIntent
+            startActivity(arrowIntent)
+        }
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if (hasFocus) {
+            supportButton.clearColorFilter()
+            arrowButton.clearColorFilter()
+            shareButton.clearColorFilter()
+
+        } else if (currentNightMode == Configuration.UI_MODE_NIGHT_NO) {
+            when (intentValue?.action) {
+                Intent.ACTION_SENDTO -> supportButton.setColorFilter(ContextCompat.getColor(this, R.color.YP_black))
+                Intent.ACTION_SEND -> shareButton.setColorFilter(ContextCompat.getColor(this, R.color.YP_black))
+                Intent.ACTION_VIEW -> arrowButton.setColorFilter(ContextCompat.getColor(this, R.color.YP_black))
+            }
+        }
+    }
 }
